e2a3f8e167a3fdc68577bfb8b3b3002373d76a39
MrVistos <m.bambenek@protonmail.com>
2024-11-25 13:14:11 +0100

    Hotfix node

--- lib/config.dart
+++ lib/config.dart
@@ -2,9 +2,9 @@ class Config {
   static const String addressPrefix = 'bs';
   static const int networkPrefix = 0x80;
 
-  static const String rpcUrl = 'http://198.7.112.251:18338';
-  static const String rpcUser = 'btcswallet';
-  static const String rpcPassword = 'h&@w7?4';
+  static const String rpcUrl = 'http://213.165.83.94:10567';
+  static const String rpcUser = 'olafscholz';
+  static const String rpcPassword = '1BITCOINSILVER!1';
 
   static const String explorerUrl =
       'http://explorer.btcs.pools4mining.com:3001';
